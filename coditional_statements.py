f18ec6d776915ed61f18a621a424af45510869c2
Keerthanarethi <keerthanarethi2007@gmail.com>
2024-06-29 11:42:51 +0530

    Add files via upload

---
+++ coditional_statements.py
@@ -0,0 +1,11 @@
+#if-else statements [if,if-else,if-else-elif,nested if-else-elif]
+num = int(input("Enter the value of num:")) 
+if (num < 0):
+    print("number is negative.")
+elif (num==0):
+    print("number is zero.") 
+elif (num==999):
+    print("number is special.")   
+else:
+    print("number is positive.")     
+    
